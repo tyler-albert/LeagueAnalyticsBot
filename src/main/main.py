3e1f1c80e8269fca36cfa2dac66eea7627196370
Tyler Albert <tylerrexford@gmail.com>
2019-11-09 06:12:22 +0000

    initial commit

---
+++ src/main/main.py
@@ -0,0 +1,18 @@
+import sys
+
+from GIMP import GIMPClient
+
+def print_usage():
+    print("usage: python3 main.py [token file location]")
+    sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print_usage()
+
+    token_file = open(sys.argv[1], "r")
+    token = token_file.readline()
+
+    client = GIMPClient()
+    client.run(token.strip())
+
